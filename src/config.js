9163692a146a969d732dd894340a2035d811220a
Adrian <adrian.silvasantiste@tecsup.edu.pe>
2025-11-24 03:41:23 -0500

    cambios en el estilo del mapa

--- src/config.js
+++ src/config.js
@@ -13,9 +13,91 @@ export const MAP_CONFIG = {
     fullscreenControl: true,
     styles: [
       {
+        elementType: 'geometry',
+        stylers: [{ color: '#242f3e' }]
+      },
+      {
+        elementType: 'labels.text.stroke',
+        stylers: [{ color: '#242f3e' }]
+      },
+      {
+        elementType: 'labels.text.fill',
+        stylers: [{ color: '#746855' }]
+      },
+      {
+        featureType: 'administrative.locality',
+        elementType: 'labels.text.fill',
+        stylers: [{ color: '#d59563' }]
+      },
+      {
         featureType: 'poi',
-        elementType: 'labels',
+        elementType: 'labels.text.fill',
         stylers: [{ visibility: 'off' }]
+      },
+      {
+        featureType: 'poi.park',
+        elementType: 'geometry.fill',
+        stylers: [{ color: '#415a2a' }]
+      },
+      {
+        featureType: 'poi.park',
+        elementType: 'labels.text.fill',
+        stylers: [{ color: '#9e9c84' }]
+      },
+      {
+        featureType: 'road',
+        elementType: 'geometry',
+        stylers: [{ color: '#38414e' }]
+      },
+      {
+        featureType: 'road',
+        elementType: 'geometry.stroke',
+        stylers: [{ color: '#212a37' }]
+      },
+      {
+        featureType: 'road',
+        elementType: 'labels.text.fill',
+        stylers: [{ color: '#9ca5b3' }]
+      },
+      {
+        featureType: 'road.highway',
+        elementType: 'geometry',
+        stylers: [{ color: '#746855' }]
+      },
+      {
+        featureType: 'road.highway',
+        elementType: 'geometry.stroke',
+        stylers: [{ color: '#1f2835' }]
+      },
+      {
+        featureType: 'road.highway',
+        elementType: 'labels.text.fill',
+        stylers: [{ color: '#f3751b' }]
+      },
+      {
+        featureType: 'transit',
+        elementType: 'geometry',
+        stylers: [{ color: '#2f3948' }]
+      },
+      {
+        featureType: 'transit.station',
+        elementType: 'labels.text.fill',
+        stylers: [{ color: '#d59563' }]
+      },
+      {
+        featureType: 'water',
+        elementType: 'geometry.fill',
+        stylers: [{ color: '#17263c' }]
+      },
+      {
+        featureType: 'water',
+        elementType: 'labels.text.fill',
+        stylers: [{ color: '#515c6d' }]
+      },
+      {
+        featureType: 'water',
+        elementType: 'labels.text.stroke',
+        stylers: [{ color: '#17263c' }]
       }
     ]
   }
